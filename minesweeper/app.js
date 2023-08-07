44ce8200fe18599268a58064d3d4197575482406
travissouthard <tsouthard88@gmail.com>
2023-08-06 21:57:07 -0400

    Refactor board to build from array of values

--- minesweeper/app.js
+++ minesweeper/app.js
@@ -2,17 +2,31 @@ const width = 10;
 const boardSize = width * width;
 const bombCount = 20;
 const validCount = boardSize - bombCount;
+const squareValues = [];
+
+for (let i = 0; i < boardSize; i++) {
+    if (i < bombCount) {
+        squareValues.push("bomb");
+    } else {
+        squareValues.push("valid");
+    }
+}
 
 const board = document.getElementById("board");
 
-for (let i = 0; i < 100; i++) {
-    const clickedClass = "clicked";
-    const square = document.createElement("p");
-    square.innerHTML = `${i + 1}`;
-    square.addEventListener("click", (e) => {
-        if (!square.classList.contains(clickedClass)) {
-            square.classList.add(clickedClass);
-        }
-    });
-    board.appendChild(square);
-}
+const createBoard = () => {
+    for (let value of squareValues) {
+        const clickedClass = "clicked";
+        const square = document.createElement("p");
+        square.innerHTML = value === "bomb" ? "💣" : "😀";
+        square.classList.add(value);
+        square.addEventListener("click", (e) => {
+            if (!square.classList.contains(clickedClass)) {
+                square.classList.add(clickedClass);
+            }
+        });
+        board.appendChild(square);
+    }
+};
+
+createBoard();
