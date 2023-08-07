b703a3d2f40bbcceff3293c57934ff52c37f673a
travissouthard <tsouthard88@gmail.com>
2023-08-06 21:10:20 -0400

    Create a board with 100 things in it

--- minesweeper/app.js
+++ minesweeper/app.js
@@ -1,1 +1,12 @@
-console.log("This is connected");
+const width = 10;
+const boardSize = width * width;
+const bombCount = 20;
+const validCount = boardSize - bombCount;
+
+const board = document.getElementById("board");
+
+for (let i = 0; i < 100; i++) {
+    const square = document.createElement("p");
+    square.innerHTML = `${i + 1}`;
+    board.appendChild(square);
+}
