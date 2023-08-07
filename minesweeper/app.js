f5ef6fb23bd72982d2c83712c595e4ed1be17c35
travissouthard <tsouthard88@gmail.com>
2023-08-06 21:37:37 -0400

    Make the squares clickable

--- minesweeper/app.js
+++ minesweeper/app.js
@@ -6,7 +6,13 @@ const validCount = boardSize - bombCount;
 const board = document.getElementById("board");
 
 for (let i = 0; i < 100; i++) {
+    const clickedClass = "clicked";
     const square = document.createElement("p");
     square.innerHTML = `${i + 1}`;
+    square.addEventListener("click", (e) => {
+        if (!square.classList.contains(clickedClass)) {
+            square.classList.add(clickedClass);
+        }
+    });
     board.appendChild(square);
 }
